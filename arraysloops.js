45860cf46d08bc2ca21c495fb6fa07ffb468a8ce
Zahra Jafaripoor <zahrajafaripoor1394@gmail.com>
2024-08-06 14:55:44 +0100

    activity 1

--- arraysloops.js
+++ arraysloops.js
@@ -1,6 +1,47 @@
-let coffeOrder = [
-    "Alex - cortado", 
-    "Ben - Latte",
-    "Charlie - whatever's new"
-]
-console.log (coffeOrder)
+//let coffeOrder = [
+    //"Alex - cortado", 
+   // "Ben - Latte",
+    //"Charlie - whatever's new"
+//]
+//console.log (coffeOrder[2]);
+//console.log (coffeOrder[1]);
+
+//coffeOrder[1] = "Ann - Hot Choc"
+//console.log (coffeOrder);
+
+//console.log (coffeOrder ,length);
+
+//coffeOrder.push("Donna - Espresso")
+//console.log (coffeOrder);
+//console.log (coffeOrder ,length);
+
+
+
+//Activity 1
+
+// Step 1: array with 3 favorite Adele songs
+let adeleSongs = [
+    "Someone Like You", 
+    "Rolling in the Deep", 
+    "Hello"];
+
+
+console.log("Initial Adele Songs:", adeleSongs);
+
+// Step 3: Add two more songs to the array
+adeleSongs.push("When We Were Young",
+     "Set Fire to the Rain");
+
+
+console.log("Updated Adele Songs:", adeleSongs);
+
+let favSongs = 1
+    "Someone Like You"
+    "Rolling in the Deep", 
+    "Hello"
+    for (let i=0; i < favSongs.length ; i++)[
+        console.log(favSongs[i])
+    ]
+
+   // ACTIVITY 2
+   
